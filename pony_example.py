ae848b4754b56b3cd984b291df4372b6d42e3d68
Colin McCloskey <colin@arcweb.co>
2020-04-27 01:01:55 -0400

    sample queries

--- pony_example.py
+++ pony_example.py
@@ -7,10 +7,9 @@ from pony.orm import *
 
 
 db = Database()
-db.bind(provider='sqlite', filename=':memory:')
 
 
-class LogEntityEvents(db.Entity):
+class LogEntityEvents():
     def print_log(self, msg: str):
         print(msg, self)
 
@@ -45,12 +44,28 @@ def on_connect(db, connection):
     print("-> Database:", db)
     print("-> Connection:", connection)
 
+db.bind(provider='sqlite', filename=':memory:')
+
+db.generate_mapping(create_tables=True)
+
+with db_session():
+    f = Family(name="Hart")
+    p = Person(family=f, name="Jim", age="69", gender="male")
+    p = Person(family=f, name="Bee", age="69", gender="female")
     f = Family(name="McCloskey")
     p = Person(family=f, name="Colin", age=37, gender="male")
     p = Person(family=f, name="Liz", age=36, gender="female")
     p = Person(family=f, name="Bennett", age=6, gender="male")
     p = Person(family=f, name="Lila", age=2, gender="female")
 
+    n_families = count(f for f in Family)
+    n_people = count(p for p in Person)
+    n_mccloskey = count(p for p in Person if p.family == f)
 
-db.bind(**options)
-db.generate_mapping(create_tables=True)
+    print("There are", n_people, "people in", n_families, "families")
+
+    for f in select(f for f in Family):
+        n = count(p for p in Person if p.family == f)
+        a = avg(p.age for p in Person if p.family == f)
+        print("There are", n, "members of the", f.name, "family, with an average age of", a)
+
